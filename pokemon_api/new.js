2fb2e0186978e6c0f1cdacf37181709d5bfb0e78
Nidhi Jain <85149221+nidhi-jain-12@users.noreply.github.com>
2021-09-01 11:53:32 +0530

    Add files via upload

---
+++ pokemon_api/new.js
@@ -0,0 +1,60 @@
+const express=require('express');
+
+const mongoose=require('mongoose');
+const app=express();
+
+
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27012/pokemonapi",{useNewUrlParser:true},()=>{
+    console.log("Mongodb connceted !!");
+});
+
+const apiSchema=new mongoose.Schema({
+    name:String,
+    species:String,
+    type:String,
+    imageUrl:String
+});
+
+const apiModel=new mongoose.model("pokemon_api",apiSchema);
+
+app.get("/pokemon_api",async (req,res)=>{
+    let apidata=await apiModel.find();
+    res.send(apidata);
+    console.log(apidata);
+});
+
+
+app.get("/pokemon_api/:id",async (req,res)=>{
+
+    let id=req.params.id;
+    let pokemonid=await apiModel.find({_id:id})
+    res.send(pokemonid);
+
+});
+
+app.get("pokemon_api/type/:type",async (req,res)=>{
+    let type=req.params.type;
+    let pokemontype=await apiModel.find({type:type});
+    res.send(pokemontype);
+});
+
+
+app.post("pokemon_api",(req,res)=>{
+
+    let pokemon=req.body;
+
+    let pokemonObj=new apiModel(pokemon);
+
+    pokemonObj.save((err,data)=>{
+        if(err===null){
+            res.send({message:"Pokemon Created"});
+        }
+    });
+
+});
+
+app.listen(8000,()=>{
+    console.log("Server is running !!");
+});
